8f1170685d00f269b7bc1b9ee8934c05d33178e1
nebiyu-ethio <nebiyuethio@gmail.com>
2024-07-17 21:33:39 +0300

    modularize terraform project

--- main.tf
+++ main.tf
@@ -2,15 +2,6 @@ provider "aws" {
     region = "us-east-1"
 }
 
-variable vpc_cidr_block {}
-variable subnet_cidr_block {}
-variable avail_zone {}
-variable env_prefix {}
-variable my_ip {}
-variable instance_type {}
-variable public_key_location {}
-variable private_key_location {}
-
 resource "aws_vpc" "myapp-vpc" {
     cidr_block = var.vpc_cidr_block
     tags = {
@@ -18,131 +9,23 @@ resource "aws_vpc" "myapp-vpc" {
     }
 }
 
-resource "aws_subnet" "myapp-subnet-1" {
-    vpc_id = aws_vpc.myapp-vpc.id 
-    cidr_block = var.subnet_cidr_block
-    availability_zone = var.avail_zone
-    tags = {
-        Name: "${var.env_prefix}-subnet-1"
-    }
-}
-
-resource "aws_route_table" "myapp-route-table" {
-    vpc_id = aws_vpc.myapp-vpc.id 
-    
-    route {
-        cidr_block = "0.0.0.0/0"
-        gateway_id = aws_internet_gateway.myapp-igw.id
-    }
-    tags = {
-        Name: "${var.env_prefix}-rtb"
-    }
-}
-
-resource "aws_internet_gateway" "myapp-igw" {
+module "myapp-subnet" {
+    source = "./modules/subnet"
+    subnet_cidr_block = var.subnet_cidr_block
+    avail_zone = var.avail_zone
+    env_prefix = var.env_prefix
     vpc_id = aws_vpc.myapp-vpc.id
-    tags = {
-        Name: "${var.env_prefix}-igw"
-    }
+    default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
 }
 
-resource "aws_route_table_association" "a-rtb-subnte" {
-    subnet_id = aws_subnet.myapp-subnet-1.id
-    route_table_id = aws_route_table.myapp-route-table.id
-}
-
-resource "aws_security_group" "myapp-sg" {
-    name = "myapp-sg"
-    vpc_id = aws_vpc.myapp-vpc.id 
-
-    ingress {
-        from_port = 22
-        to_port = 22
-        protocol = "tcp"
-        cidr_blocks = [var.my_ip]
-    }
-
-    ingress {
-        from_port = 8080
-        to_port = 8080
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-
-    egress {
-        from_port = 0
-        to_port = 0
-        protocol = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-        prefix_list_ids = []
-    }
-
-    tags = {
-        Name: "${var.env_prefix}-sg"
-    }
-}
-
-data "aws_ami" "latest-mi-linkux-image" {
-    most_recent = true
-    owners = ["amazon"]
-    filter {
-        name = "name"
-        values = ["amzn2-ami-hvm-*-x86_64-gp2"]
-    }
-
-    filter {
-        name = "virtualization-type"
-        values = ["hvm"]
-    }
-}
-
-output "aws_ami_id" {
-    value = data.aws_ami.latest-mi-linkux-image.id 
-}
-
-output "ec2_public_ip" {
-    value = aws_instance.myapp-server.public_ip
-}
-
-resource "aws_key_pair" "ssh-key" {
-    key_name = "ssh-key-pair"
-    public_key = file(var.public_key_location)
-}
-
-resource "aws_instance" "myapp-server" {
-    ami = data.aws_ami.latest-mi-linkux-image.id
+module "myapp-server" {
+    source = "./modules/webserver"
+    vpc_id = aws_vpc.myapp-vpc.id
+    my_ip = var.my_ip
+    env_prefix = var.env_prefix
+    image_name = var.image_name
+    subnet_id = module.myapp-subnet.subnet.id
+    avail_zone = var.avail_zone
+    public_key_location = var.public_key_location
     instance_type = var.instance_type
-
-    subnet_id = aws_subnet.myapp-subnet-1.id
-    vpc_security_group_ids = [aws_security_group.myapp-sg.id]
-    availability_zone = var.avail_zone
-
-    associate_public_ip_address = true
-    key_name = aws_key_pair.ssh-key.key_name
-
-    #user_data = file("entry-script.sh")
-
-    connection {
-        type = "ssh"
-        host = self.public_ip
-        user = "ec2-user"
-        private_key = file(var.private_key_location)
-    }
-
-    provisioner "file" {
-        source = "entry-script.sh"
-        destination = "/home/ec2-user/entry-script-on-ec2.sh"
-    }
-
-    provisioner "remote-exec" {
-        script = file("entry-script.sh")
-    }
-
-    provisioner "local-exec" {
-        command = "echo ${self.public_ip} > output.txt"
-    }
-
-    tags = {
-        Name: "${var.env_prefix}-server"
-    }
 }
